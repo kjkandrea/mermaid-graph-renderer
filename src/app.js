d00f1731a4752b3274f2c7a5d19c659bf01f46d4
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 17:00:02 +0900

    Add structure

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
 import { render } from './api/mermaidRenderer'
 
-const main = () => {
-  const mermaidElement = document.getElementById('mermaid')
-  const content = 'flowchart\n1-- 1 ---2\n2-- 1 ---3\n'
-  render(mermaidElement, content)
+const main = {
+  init() {
+    const graph = this.maidGraph()
+    this.render(graph)
+  },
+  maidGraph() {
+    return 'flowchart\n1-- 1 ---2\n2-- 1 ---3\n'
+  },
+  render(graph) {
+    const mermaidElement = document.getElementById('mermaid')
+    render(mermaidElement, graph)
+  }
 }
-document.addEventListener('DOMContentLoaded', () => main())
+document.addEventListener('DOMContentLoaded', () => main.init())
