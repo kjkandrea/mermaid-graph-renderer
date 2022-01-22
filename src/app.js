a597c2f59e4e871d99c5ad6c52cb3e805fe59501
jongkeun-kim <kjkandrea@gmail.com>
2022-01-22 15:57:25 +0900

    Add use mermaid test

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+import mermaid from 'mermaid'
 import { render } from './api/mermaidRenderer.js'
 
 const main = () => {
   const mermaidElement = document.getElementById('mermaid')
 
   render(mermaidElement)
+
+  mermaid.initialize({
+    startOnLoad: true
+  })
 }
 document.addEventListener('DOMContentLoaded', () => main())
